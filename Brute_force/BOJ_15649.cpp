341169e6e97d0a4b59f1c1ae39e1bb4f50886494
dooli1971039 <dooli9819@gmail.com>
2021-12-26 22:45:19 +0900

    N과 M (1) -- ing

---
+++ Brute_force/BOJ_15649.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+bool isused[8];
+int solve(int level) {
+
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int n,m;
+	cin >> n >> m;
+
+	
+}
