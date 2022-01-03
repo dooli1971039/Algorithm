0ba87f0fdc24ebd340f1dd0786bc4dbf31de0e56
dooli1971039 <dooli9819@gmail.com>
2022-01-03 21:54:19 +0900

    회의실 배정 주석 추가

--- Greedy/BOJ_1931.cpp
+++ Greedy/BOJ_1931.cpp
@@ -17,17 +17,17 @@ int main() {
 		v.push_back({ b,a }); //{끝나는 시간, 시작 시간}
 	}
 
-	sort(v.begin(), v.end()); //오름차순으로 정렬
+	sort(v.begin(), v.end()); //오름차순으로 정렬 (끝나는시간이 빠른 순서대로 정렬됨)
 
 	b = 0, cnt = 0;
 	for (int i = 0; i < n; i++) {
 		if (v[i].second < b) //시작하는 시간이 이전 것의 끝나는 시간보다 앞설 때
 			continue;
 
-		cnt++;
+		cnt++; //회의 개수 카운트
 		b = v[i].first;
 	}
 
+	//결과 출력
 	cout << cnt << '\n';
-
 }
