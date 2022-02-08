1236f6b0906cd3e2d3862d664eb5f188c21757f7
dooli1971039 <dooli9819@gmail.com>
2022-02-08 20:28:31 +0900

    문자열 집합

---
+++ Others/BOJ_14425.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm> //binary_search() : 정렬된 상태에서 사용
+#include <vector>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	//입력
+	int n, m;
+	cin >> n >> m;
+	vector<string>v1(n); //듣도 못한 사람
+	vector<string>result;
+	for (int i = 0; i < n; i++) cin >> v1[i];
+
+	//문제 해결
+	sort(v1.begin(), v1.end());
+
+	string mm;
+	int count = 0;
+	for (int i = 0; i < m; i++) {
+		cin >> mm;
+		if (binary_search(v1.begin(), v1.end(), mm)) count++;
+	}
+
+	//결과 출력
+	cout << count << '\n';
+}
