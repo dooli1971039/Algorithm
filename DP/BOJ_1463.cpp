ad5cc5db16b0d2bab71e8eae06262654359bc7ea
dooli1971039 <dooli9819@gmail.com>
2022-02-26 11:56:56 +0900

    1로 만들기

---
+++ DP/BOJ_1463.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+//실패
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	//입력
+	int n;
+	cin >> n ; // 1~10^6
+	vector<int>dp(n+1,0);
+	
+	dp[1] = 0; //연산 필요 없음
+	dp[2] = 1;
+	dp[3] = 1;
+	
+	for (int i = 4; i < n + 1; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0) dp[i] = min(dp[i / 3] + 1, dp[i]);
+		if (i % 2 == 0)dp[i] = min(dp[i / 2] + 1, dp[i]);
+	}
+
+	cout << dp[n]<< '\n';
+
+}
