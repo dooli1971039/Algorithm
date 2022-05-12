96ac4a5c71561b3b1bd86e6bc2ea3f6b7360eccb
dooli1971039 <dooli9819@gmail.com>
2022-05-12 15:14:12 +0900

    좌표 정렬하기 2

---
+++ Others/BOJ_11651.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm> 
+#include <vector>
+using namespace std;
+
+bool compare(pair<int, int>a, pair<int, int>b) {
+	if (a.second == b.second) {
+		return a.first < b.first;
+	}
+	else {
+		return a.second < b.second;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	//입력
+	int n;	cin >> n;
+	vector<pair<int, int>> arr;
+
+	int x, y;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		arr.push_back(make_pair(x, y));
+	}
+
+	//정렬
+	sort(arr.begin(), arr.end(),compare);
+
+	//출력
+	for (int i = 0; i < n; i++) {
+		cout << arr[i].first << ' ' << arr[i].second << '\n';
+	}
+}
