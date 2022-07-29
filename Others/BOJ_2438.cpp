5deb098a31fdf6637fb68a31dc9f549317a66d42
dooli1971039 <dooli9819@gmail.com>
2022-07-29 21:53:45 +0900

    별 찍기 - 1

---
+++ Others/BOJ_2438.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printStar(int n){
+    for(int i=1;i<=n;i++){
+        cout<<"*";
+    }
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n; cin>>n;
+    for(int i=1;i<=n;i++){
+        printStar(i);
+        cout<<'\n';
+    }
+}
