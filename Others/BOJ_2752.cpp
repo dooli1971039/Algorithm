d825648ae02ed206551e15cd0f6b7126f98da449
dooli1971039 <dooli9819@gmail.com>
2022-08-15 21:07:05 +0900

    세수정렬

---
+++ Others/BOJ_2752.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int a[3];
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    for(int i=0;i<3;i++) cin>>a[i];
+    sort(a,a+3);
+    for(int i:a) cout<<i<<' ';
+}
