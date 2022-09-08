5eea8083de7a29553f64d16314c2f89ea5c4b0fb
dooli1971039 <dooli9819@gmail.com>
2022-09-08 20:14:54 +0900

    문자열 집합

--- Others/BOJ_14425.cpp
+++ Others/BOJ_14425.cpp
@@ -1,29 +1,32 @@
-#include <iostream>
 #include <algorithm> //binary_search() : 정렬된 상태에서 사용
+#include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	//입력
-	int n, m;
-	cin >> n >> m;
-	vector<string>v1(n); //듣도 못한 사람
-	vector<string>result;
-	for (int i = 0; i < n; i++) cin >> v1[i];
+    //입력
+    int n, m;
+    cin >> n >> m;
+    vector<string> v1(n);
+    vector<string> result;
+    for (int i = 0; i < n; i++)
+        cin >> v1[i];
 
-	//문제 해결
-	sort(v1.begin(), v1.end());
+    //문제 해결
+    sort(v1.begin(), v1.end());
 
-	string mm;
-	int count = 0;
-	for (int i = 0; i < m; i++) {
-		cin >> mm;
-		if (binary_search(v1.begin(), v1.end(), mm)) count++;
-	}
+    string mm;
+    int count = 0;
+    for (int i = 0; i < m; i++) {
+        cin >> mm;
+        if (binary_search(v1.begin(), v1.end(), mm))
+            count++;
+    }
 
-	//결과 출력
-	cout << count << '\n';
+    //결과 출력
+    cout << count << '\n';
 }
