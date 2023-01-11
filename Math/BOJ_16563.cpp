9aa79ef94967cdfc36395376a43e92431a25f3b1
dooli1971039 <dooli9819@gmail.com>
2023-01-12 06:19:59 +0900

    어려운 소인수분해

--- Math/BOJ_16563.cpp
+++ Math/BOJ_16563.cpp
@@ -1,4 +1,3 @@
-// 강의 보면서 다시 이해해보자
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -6,7 +5,7 @@ using namespace std;
 
 const int SIZE = 5e6;
 
-//원소의 가장 작은 소인수를 저장하게 된다.
+// 원소의 가장 작은 소인수를 저장하게 된다.
 vector<int> isPrime() {
     // n 이하의 수를 소인수분해하는 경로를 리턴
     vector<int> prime(SIZE + 1, 0);
@@ -16,10 +15,10 @@ vector<int> isPrime() {
         if (prime[i] != 0)
             continue;
 
-        // i 가 소수라면
+        // i 가 소수라면, 이어지는 수들을 지워야함
         for (int j = i * i; j <= SIZE; j += i) {
             if (prime[j] == 0) {
-                prime[j] = i; //어느 소수의 배수로 지워졌는지 저장
+                prime[j] = i; // 어느 소수의 배수로 지워졌는지 저장
             }
         }
     }
@@ -39,13 +38,13 @@ int main() {
     while (n--) {
         cin >> tmp;
 
-        // prime[tmp] != 0 이면 소수가 아니다.
+        // prime[tmp] != 0 이면 소수가 아니다. (소인수분해 필요)
         while (prime[tmp] != 0) {
             cout << prime[tmp] << ' ';
             tmp /= prime[tmp];
         }
 
-        // tmp 가 소수임
+        // tmp가 소수이면 자기 자신을 출력
         cout << tmp << '\n';
     }
 }
