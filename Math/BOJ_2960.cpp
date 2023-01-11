ef9e2d0b8a9a235042cc0344da223ad944f741ef
dooli1971039 <dooli9819@gmail.com>
2023-01-12 06:20:14 +0900

    에라토스테네스의 체

--- Math/BOJ_2960.cpp
+++ Math/BOJ_2960.cpp
@@ -4,21 +4,15 @@
 using namespace std;
 
 int findPrime(int n, int k) {
-    vector<bool> is_prime(n + 1, true);
-    is_prime[0] = is_prime[1] = false;
+    vector<bool> is_prime(n + 1, true); // n까지의 정수를 모두 적음
+    is_prime[0] = is_prime[1] = false;  // 0,1은 소수가 아님
 
     int cnt = 0;
     for (int i = 2; i <= n; i++) {
-        if (is_prime[i]) { //이 경우 소수라는 뜻
+        if (is_prime[i]) { // 이 경우 소수라는 뜻
             for (int j = i; j <= n; j += i) {
                 if (is_prime[j]) {
                     is_prime[j] = false;
-                    // cnt++;
-
-                    // if (cnt == k) {
-                    //     return j;
-                    // }
-
                     if (++cnt == k) {
                         return j;
                     }
