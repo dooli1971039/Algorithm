957110b3da0606b231779efb9d3086a02986072b
dooli1971039 <dooli9819@gmail.com>
2023-02-06 23:50:56 +0900

    행운의 바퀴

--- Others/BOJ_2840.cpp
+++ Others/BOJ_2840.cpp
@@ -1,9 +1,9 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 typedef pair<int, char> ci;
+const int ALPHA = 26;
 
 /*
     - 배열을 ?로 초기화 해주고, 입력한대로 처리한다
@@ -12,6 +12,9 @@ typedef pair<int, char> ci;
     <주의 사항>
     1. ?이 아닌 문자가 저장되어있는데, 다른 문자를 저장하려는 경우
     2. 룰렛에 중복되는 문자가 있는 경우
+
+    ** 바퀴를 돌리는 횟수가 매우 클 수도 있다. 이 부분을 유의하여 인덱스를 처리하자!
+    ex) n=3 인데, 바퀴를 10번 돌릴수도 있다.
 */
 
 // 특정 화살표에서 시작한 행운의 바퀴 리턴
@@ -26,8 +29,8 @@ string arrowStartWheel(int index, int n, vector<char> &wheel) {
 }
 
 string makeWheel(int n, vector<ci> &record) {
-    vector<char> wheel(n, '?');           //?로 초기화
-    vector<bool> is_available(26, false); // 알파벳 중복 체크
+    vector<char> wheel(n, '?');              //?로 초기화
+    vector<bool> is_available(ALPHA, false); // 알파벳 중복 체크
 
     int index = 0; // 화살표가 가리키는 인덱스
 
@@ -35,10 +38,7 @@ string makeWheel(int n, vector<ci> &record) {
         int rot = record[i].first;
         int ch = record[i].second;
 
-        index = index - rot;
-        if (index < 0) { // 음수일 경우에 +n 해서 인덱스를 조정(원판이니까)
-            index += n;
-        }
+        index = (index - rot + 100 * n) % n;
 
         if (wheel[index] == ch) {
             continue;
