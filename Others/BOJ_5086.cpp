81335cb95a52d1a9946abaead02125ad607e1935
dooli1971039 <dooli9819@gmail.com>
2023-03-14 19:46:08 +0900

    배수와 약수

---
+++ Others/BOJ_5086.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=-1,b=-1;
+    
+    while(true){
+        cin>>a>>b;
+        if(a==0&&b==0){
+            return 0;
+        }
+        
+        if(a%b==0){
+            cout<<"multiple\n";
+        }else if(b%a==0){
+            cout<<"factor\n";
+        }else{
+            cout<<"neither\n";
+        }
+        
+    }
+}
