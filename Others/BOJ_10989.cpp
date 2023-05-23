761fdc5e397a9625cc7eca1e5072bab4c81d1dd7
dooli1971039 <dooli9819@gmail.com>
2023-05-23 23:12:56 +0900

    수 정렬하기 3

---
+++ Others/BOJ_10989.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, num;
+	int arr[10001]={0,};
+	cin >> n;
+	
+	for(int i=0; i<n ;i++){
+		cin>>num;
+		arr[num] = arr[num] + 1; //숫자 카운트  
+	}
+	
+	for(int i=1; i<10001; i++){
+		for(int j=1; j<=arr[i]; j++){ //숫자만큼 반복 출력 
+			cout<<i<<'\n';
+		}
+	}
+}
