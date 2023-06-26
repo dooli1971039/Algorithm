c56c1b7f6e7bbfa20487a92ace78a7c4256dc9d2
dooli1917039 <dooli9819@gmail.com>
2023-06-26 22:04:22 +0900

    N과 M (2)

---
+++ Backtracking/BOJ_15650.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> result(9, 0); // 1~8까지라서
+vector<bool> used(9, false);
+
+void back(int cnt, int before) {
+    if (cnt == m) {
+        // 여기서 출력 후 return
+        for (int i = 0; i < cnt; i++) {
+            cout << result[i] << ' ';
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = before + 1; i <= n; i++) {
+        if (!used[i]) { // 사용되지 않았을 때
+            used[i] = true;
+            result[cnt] = i;
+            back(cnt + 1, i);
+            used[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    back(0, 0);
+}
