ef5201b717c03ebdd2db34960bb400eb3857418d
dooli1917039 <dooli9819@gmail.com>
2023-06-27 22:51:16 +0900

    N과 M (4)

---
+++ Backtracking/BOJ_15652.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> result(9, 0);
+
+void back(int cnt, int before) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << result[i] << ' ';
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = before; i <= n; i++) {
+        result[cnt] = i;
+        back(cnt + 1, i);
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    back(0, 1);
+}
