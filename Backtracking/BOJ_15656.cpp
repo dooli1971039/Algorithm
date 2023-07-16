598d4c5be7d097f9f0b38f09a875b0c4807003bd
dooli1917039 <dooli9819@gmail.com>
2023-07-16 22:32:10 +0900

    N과 M (7)

---
+++ Backtracking/BOJ_15656.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+
+vector<int> result(7, 0);
+vector<int> arr;
+
+void back(int cnt) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << result[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        result[cnt] = arr[i];
+        back(cnt + 1);
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    arr.assign(n, 0); // 여기서 초기화를 크기 n만큼 초기화를 할 것
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+
+    back(0);
+}
