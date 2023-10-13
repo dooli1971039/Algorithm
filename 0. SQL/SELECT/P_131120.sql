9ee2b2e88ecaf2851cb50a31e2fbea64b24b0bc9
dooli1917039 <dooli9819@gmail.com>
2023-10-13 23:44:26 +0900

    SQL select 연습

---
+++ 0. SQL/SELECT/P_131120.sql
@@ -0,0 +1,5 @@
+-- 3월에 태어난 여성 회원 목록 출력하기
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE month(DATE_OF_BIRTH) = 3 AND TLNO is not null AND GENDER='W'
+ORDER BY MEMBER_ID;
